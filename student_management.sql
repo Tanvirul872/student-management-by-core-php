0fbc6e41859c184b4493005d148b29bd8b31806f
Tanvirul <anmtanvir872@gmail.com>
2022-01-09 15:39:22 +0600

    change

--- student_management.sql
+++ student_management.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 02, 2022 at 05:50 PM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 7.3.28
+-- Generation Time: Jan 09, 2022 at 10:38 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.3.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -37,6 +37,13 @@ CREATE TABLE `student_info` (
   `datetime` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `student_info`
+--
+
+INSERT INTO `student_info` (`id`, `name`, `roll`, `class`, `pcontact`, `photo`, `datetime`) VALUES
+(4, 'tanvir Karim', '1234', '1st', '03456778888', '1034.jpg', '2022-01-03 09:09:26');
+
 -- --------------------------------------------------------
 
 --
@@ -50,17 +57,18 @@ CREATE TABLE `users` (
   `username` varchar(50) NOT NULL,
   `password` varchar(100) NOT NULL,
   `photo` varchar(50) NOT NULL,
-  `status` varchar(10) NOT NULL
+  `status` varchar(10) NOT NULL,
+  `datetime` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `username`, `password`, `photo`, `status`) VALUES
-(1, 'tanvir', 'anmtanvir872@gmail.com', 'admin', '123', '', ''),
-(5, 'Tanvirul Karim', 'admin@gmail.com', 'author', '12345', 'author.jpg', 'active'),
-(6, 'tanvir Karim', 'anmtanvir@gmail.com', 'author1', 'sss', 'author1.', 'inactive');
+INSERT INTO `users` (`id`, `name`, `email`, `username`, `password`, `photo`, `status`, `datetime`) VALUES
+(1, 'tanvir', 'anmtanvir872@gmail.com', 'admin', '123', '', '', '2022-01-09 08:19:39'),
+(5, '[value-2]', 'admin@gmail.com', 'author', '12345', 'author.jpg', 'active', '2022-01-09 08:19:39'),
+(6, 'tanvir Karim', 'anmtanvir@gmail.com', 'author1', 'sss', 'author1.', 'inactive', '2022-01-09 08:19:39');
 
 --
 -- Indexes for dumped tables
@@ -88,7 +96,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `student_info`
 --
 ALTER TABLE `student_info`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `users`
